ad1b727a3779ef3dc35ff1fa5a18e54efa4640f1
Yuttapichai Lamnaonan <y.lamnaonan@gmail.com>
2021-04-02 02:57:51 +0000

    update config and add environment variable file

--- config/config.go
+++ config/config.go
@@ -1,8 +1,22 @@
 package config
 
-var (
-	KeycloakHost         = "http://192.168.1.99:8080"
-	KeycloakRealm        = "aigen"
-	KeycloakClientID     = "aigen-client"
-	KeycloakClientSecret = "8dfddd7c-00dc-44d9-b437-723c9c84118f"
+import (
+	"fmt"
+	"os"
 )
+
+var (
+	KeycloakHost         = getEnv("KEYCLOAK_HOST", "http://localhost:8000")
+	KeycloakRealm        = getEnv("KEYCLOAK_REALM", "master")
+	KeycloakClientID     = getEnv("KEYCLOAK_CLIENT_ID", "default")
+	KeycloakClientSecret = getEnv("KEYCLOAK_CLIENT_SECRET", "")
+)
+
+func getEnv(key string, defaultValue string) string {
+	val, exist := os.LookupEnv(key)
+	if !exist {
+		val = defaultValue
+	}
+	fmt.Println(val)
+	return val
+}
